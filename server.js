fda35e21ef93e9858003b62247376dad890ba490
Eugene Breslavskiy <Eugebebr.92@gmail.com>
2019-06-17 12:37:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const WebSocket = require('ws')
 
-const server = new WebSocket.Server({ port: 3000 })
+const server = new WebSocket.Server({ port: 3001 })
 
 server.on('connection', ws => {
-    ws.send('Подключение к серверу завершено')
-
     ws.on('message', message => {
         server.clients.forEach(client => {
             if (client.readyState == WebSocket.OPEN) {
                 client.send(message)
-
-                console.log('Пришло сообщение с клиента: ' + message)
+                console.log(message)
             }
         })
     })
 
-    console.log('Подключение к серверу завершено')
+    console.log('Открыли соединение')
 })
